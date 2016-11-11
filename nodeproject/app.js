7f818f291e8591d57ff8101f43215d2e1c4e1d7f
Zachary Tribbett <tribbettz+c9@gmail.com>
2016-11-11 05:40:29 +0000

    adding bower, gulp, theming.

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,20 @@
-var aloha = 'Aloha Variables';
-console.log(aloha);
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.use(express.static('bower_components'));
+
+app.get('/', function(req, res) {
+    res.send('Aloha Zach');
+});
+
+app.get('/routing', function(req, res) {
+    res.send('Aloha Routing');
+});
+
+app.listen(port, function(err) {
+    console.log('The Server is Running on port: ' + port);
+});
